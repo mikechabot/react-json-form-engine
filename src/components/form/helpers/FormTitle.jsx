4e9fc999e8b3c00fb45fe3b496d4eb6087a460d9
Mike <mike.chabot@live.com>
2018-05-27 23:51:45 -0400

    Leaving submit on mobile

--- src/components/form/helpers/FormTitle.jsx
+++ src/components/form/helpers/FormTitle.jsx
@@ -1,93 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Icon from '../../common/Icon';
+import { Icon, Flex } from '../../common';
 
 const DEFAULT_THEME = 'is-dark';
-const className = 'navbar-item is-size-4-desktop is-size-5-tablet is-size-6-mobile';
+const className = 'navbar-item is-size-4-desktop is-size-5-tablet is-size-5-mobile';
 
-class FormTitle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showMenu: false
-        };
-        this._toggleMenu = this._toggleMenu.bind(this);
-    }
-    render() {
-        const { id, theme, controlsRight } = this.props;
-        return (
-            <nav id={id} className={`navbar is-transparent ${theme || DEFAULT_THEME}`}>
-                <div className="navbar-brand">
-                    {this._renderBrand()}
-                    {this._maybeRenderControlsRightContainer(controlsRight)}
-                </div>
-                {this._maybeRenderControlsRight(controlsRight)}
-            </nav>
-        );
-    }
+const FormTitle = ({ id, label, icon, iconPrefix, theme, controlsRight }) => {
+    return (
+        <Flex
+            id={id}
+            vAlignCenter
+            flexShrink={0}
+            justifyContent="space-between"
+            className={`navbar ${theme || DEFAULT_THEME}`}
+        >
+            <div className="navbar-brand">
+                <span className={className}>
+                {_maybeRenderIcon(icon, iconPrefix)}
+                {_renderLabel(label)}
+                </span>
+            </div>
+            {_maybeRenderControlsRight(controlsRight)}
+        </Flex>
+    );
+};
 
-    _renderBrand() {
-        const { url } = this.props;
-        const children = this._renderIconAndLabel();
-        if (url) {
-            return (
-                <a href={url} className={className}>
-                    {children}
-                </a>
-            );
-        }
-        return <span className={className}>{children}</span>;
-    }
-
-    _renderIconAndLabel() {
-        const { icon, iconPrefix, label } = this.props;
-        return [_renderIcon(icon, iconPrefix), _renderSpacer(), _renderLabel(label)];
-    }
-
-    _maybeRenderControlsRightContainer(controls) {
-        if (controls) {
-            return (
-                <div
-                    className={`navbar-burger burger ${this.state.showMenu ? 'is-active' : ''}`}
-                    data-target="navbar-controls-right"
-                    onClick={this._toggleMenu}
-                >
-                    <span />
-                    <span />
-                    <span />
-                </div>
-            );
-        }
-    }
-    _maybeRenderControlsRight(controls) {
-        if (controls) {
-            return (
-                <div
-                    id={`${this.props.id}-controls-right`}
-                    className={`navbar-menu ${this.state.showMenu ? 'is-active' : ''}`}
-                >
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="field is-grouped">
-                                <div className="control">{controls}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-    }
-    _toggleMenu() {
-        this.setState({ showMenu: !this.state.showMenu });
-    }
-}
-
-const _renderIcon = (icon, iconPrefix) => {
+const _maybeRenderIcon = (icon, iconPrefix) => {
     if (icon) {
         return (
             <span key="icon">
-                <Icon icon={icon} prefix={iconPrefix} />
+                <Icon icon={icon} prefix={iconPrefix} />&nbsp;
             </span>
         );
     }
@@ -97,16 +40,19 @@ const _renderLabel = label => {
     return <span key="label">{label}</span>;
 };
 
-const _renderSpacer = () => <span key="spacer">&nbsp;</span>;
+const _maybeRenderControlsRight = controls => {
+  if (controls) {
+    return <div className="navbar-item">{controls}</div>;
+  }
+};
 
 FormTitle.propTypes = {
     id: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-    theme: PropTypes.string,
-    iconPrefix: PropTypes.string,
     label: PropTypes.node.isRequired,
-    controlsRight: PropTypes.node,
-    url: PropTypes.string
+    icon: PropTypes.string,
+    iconPrefix: PropTypes.string,
+    theme: PropTypes.string,
+    controlsRight: PropTypes.node
 };
 
 export default FormTitle;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-export { default as Form } from './components/form/Form';
-export { default as FormEngine } from './form/form-engine';
+export {default as Form } from './components/form/Form';
+export { default as FormEngine }from './form/form-engine';
+
