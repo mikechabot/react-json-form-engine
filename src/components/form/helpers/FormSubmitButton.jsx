d0301df8e3e99fa25c577217d746ee7e8b4b2190
Mike Chabot <mike.chabot@live.com>
2018-06-03 21:14:44 -0400

    Fixing submit button label prorp

--- src/components/form/helpers/FormSubmitButton.jsx
+++ src/components/form/helpers/FormSubmitButton.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FormSubmitButton = ({ onSubmit, submitButtonLabel }) => (
+const FormSubmitButton = ({ onSubmit, label }) => (
     <button className="button is-link" onClick={onSubmit}>
-        {submitButtonLabel || 'Submit'}
+        {label || 'Submit'}
     </button>
 );
 
 FormSubmitButton.propTypes = {
     onSubmit: PropTypes.func.isRequired,
-    submitButtonLabel: PropTypes.string
+    label: PropTypes.string
 };
 
 export default FormSubmitButton;
